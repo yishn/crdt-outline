247b4267fa9d8fbb59fcf9334a0bce2381a98642
Yichuan Shen <shen.yichuan@gmail.com>
2019-09-27 17:09:12 +0200

    Add insertItem()

--- src/Outline.js
+++ src/Outline.js
@@ -7,28 +7,30 @@ class Outline extends EventEmitter {
   constructor(authorId) {
     super()
 
-    this.authorId = authorId || uuid()
+    this.author = authorId || uuid()
     this.timestamp = 0
 
     this._items = {}
     this._childrenMap = {[null]: []}
     this._parentMap = {}
+
+    this.compareId = compareMap(id => this.getItem(id).position, px.compare)
   }
 
-  getItemId() {
-    return JSON.stringify([this.timestamp++, this.authorId])
+  createItemId() {
+    return JSON.stringify([this.timestamp++, this.author])
   }
 
-  getNextItemId(id) {
+  getNextItem(id) {
     let children = this.getChildren(id)
-    if (children.length > 0) return children[0].id
+    if (children.length > 0) return children[0]
 
     let inner = id => {
       let parent = this.getParent(id)
-      let siblings = this.getChildren(parent.id)
+      let siblings = this.getChildren(parent == null ? null : parent.id)
 
       let index = siblings.findIndex(item => item.id === id)
-      if (index + 1 < siblings.length) return siblings[index + 1].id
+      if (index + 1 < siblings.length) return siblings[index + 1]
 
       return inner(parent.id)
     }
@@ -41,11 +43,48 @@ class Outline extends EventEmitter {
   }
 
   getParent(id) {
-    return this.getItem(this._parentMap[id])
+    return this._parentMap[id] == null ? null : this.getItem(this._parentMap[id])
   }
 
   getChildren(id) {
     return this._childrenMap[id].map(childId => this.getItem(childId))
+  }
+
+  _insertItem(parentId, item) {
+    if (parentId != null && this.getItem(parentId) == null) {
+      return false
+    }
+
+    this._items[item.id] = item
+    this._childrenMap[item.id] = []
+    this._parentMap[item.id] = parentId
+
+    this._childrenMap[parentId].push(item.id)
+    this._childrenMap[parentId].sort(this.compareId)
+
+    return true
+  }
+
+  insertItem(parentId, beforeId, content) {
+    if (beforeId == null) {
+      beforeId = parentId
+    }
+
+    let id = this.createItemId()
+    let beforeItem = this.getItem(beforeId)
+    let afterItem = this.getNextItem(beforeId)
+    let position = px.create(this.author, beforeItem.position, afterItem != null ? afterItem.position : null)
+    let item = {id, position, content}
+    let result = this._insertItem(parentId, item)
+
+    this.emit('change', {
+      change: {
+        type: '_insertItem',
+        args: [parentId, item]
+      }
+    })
+
+    return result
   }
 
   applyChanges(changes) {
